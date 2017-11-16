4ccfe4ec67eff80d3bdf8a2dd228a3cb8a48a111
Ryan Bril <rb@cm.nl>
2017-11-16 12:35:13 +0100

    Removed validation on SOFORT banknumber, bic and consumer name

--- Source/CM.Payments.Client.Shared/Validators/SofortDetailsValidator.cs
+++ Source/CM.Payments.Client.Shared/Validators/SofortDetailsValidator.cs
@@ -7,11 +7,6 @@ namespace CM.Payments.Client.Validators
     {
         public SofortDetailsValidator()
         {
-            this.RuleFor(r => r.PurchaseId).NotEmpty().Length(1, 35);
-            this.RuleFor(r => r.BankAccountNumber).NotEmpty().Length(1, 34);
-            this.RuleFor(r => r.ConsumerName).NotEmpty().Length(1, 255);
-            this.RuleFor(r => r.BankBic).NotEmpty().Length(1, 11);
-            this.RuleFor(r => r.PurchaseId).NotEmpty().Length(1, 35);
             this.RuleFor(r => r.Description).NotEmpty().Length(1, 35);
         }
     }
