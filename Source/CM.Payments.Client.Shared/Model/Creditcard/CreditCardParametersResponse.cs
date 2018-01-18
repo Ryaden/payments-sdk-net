60277804f6c8a6997983ebc3173cc9d638fa9130
Ryan Bril <rb@cm.nl>
2018-01-18 13:12:38 +0100

    Added parameter response for cc

---
+++ Source/CM.Payments.Client.Shared/Model/Creditcard/CreditCardParametersResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CM.Payments.Client.Model.Creditcard
+{
+    public class CreditCardParametersResponse
+    {
+        public string MD { get; set; }
+        public string PaReq { get; set; }
+        public string TermUrl { get; set; }
+    }
+}
